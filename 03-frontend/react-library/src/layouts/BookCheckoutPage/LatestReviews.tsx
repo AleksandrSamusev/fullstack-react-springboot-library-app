d33932a952bb28261fcf00a9525a93fefe81be49
Александр Самусев <98308197+AleksandrSamusev@users.noreply.github.com>
2023-12-27 07:08:02 +0100

    Add Shelf Page. Closes #6

--- 03-frontend/react-library/src/layouts/BookCheckoutPage/LatestReviews.tsx
+++ 03-frontend/react-library/src/layouts/BookCheckoutPage/LatestReviews.tsx
@@ -19,7 +19,8 @@ export const LatestReviews: React.FC<{
                         ))}
 
                         <div className="m-3">
-                            <Link type="button" className="btn btn-md main-color text-white" to='#'>
+                            <Link type="button" className="btn btn-md main-color text-white" 
+                            to={`/review-list/${props.bookId}`}>
                                 Reach all reviews.
                             </Link>
                         </div>
